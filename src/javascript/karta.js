199bdb6258c69d5a3871038c672adbb950c8f09a
Kiim94 <nemomoa@hotmail.com>
2026-03-01 15:33:34 +0100

    Uppgift sa annat. Tvungen att göra om karta

--- src/javascript/karta.js
+++ src/javascript/karta.js
@@ -10,29 +10,11 @@
  */
 window.addEventListener("load", () => {
     //hämta var kartan ska ligga/laddas in och sätt startvy. Nedan är norrmesunda
-let map = L.map("map").setView([63.29, 18.71], 15);
+    let map = L.map("map").setView([63.29, 18.71], 15);
 
-//lägg till kartlager
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-//lägg till "grundmarkör", för start. Kommer finnas hela tiden
-L.marker([63.29, 18.71]).addTo(map)
-
-//lägg till geocoder - sökruta, plugin för leaflet
-const searchCode = L.Control.geocoder({
-    collapsed:true}).addTo(map);
-
-    //.on - samma som addEventListener
-    searchCode.on("markgeocode", (event) => {
-        //skapa koordinater för sökresultat - latitut och longitud. Flyttar kartan dit. 
-        //Zoomar in på 15. Lägger till markör o popup med namn
-        //coords fungerar ungefär som JSON, fast array. Hämtar latitud och longitud
-        const coords = [event.geocode.center.lat, event.geocode.center.lng];
-        map.setView(coords, 15);
-        L.marker(coords).addTo(map).bindPopup(event.geocode.name).openPopup();
-    })
-
+    //lägg till kartlager
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 });
